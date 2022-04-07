47d1f830a8db54bd0eb305ad840217df18a99f86
Rokibul Islam <rokibul630@gmail.com>
2022-04-07 15:17:58 +0530

    add sql to create user table

---
+++ users.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `created_at` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
